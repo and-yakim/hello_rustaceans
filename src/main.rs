c1766abe22048e5fe16f8c7b612ef9c5c8eeeef7
Andrey Yakimenko <59168235+and-yakim@users.noreply.github.com>
2024-03-03 23:33:54 +0200

    u8x64 simd - compiler will divide vectors into real life simd

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use bitvec::prelude::*;
 use minifb::{Key, Window, WindowOptions};
 use rand::random;
 use rayon::prelude::*;
-use std::simd::{u8x16, Simd};
+use std::simd::{u8x64, Simd};
 use std::{thread, time};
 
 #[allow(dead_code)]
@@ -62,15 +62,15 @@ const GREY_SHADES: [u32; 5] = {
 
 const ROWS_: isize = ROWS as isize;
 const COLS_USIZE: usize = COLS / 64;
-const CHUNK_SIZE: usize = 16;
+const CHUNK_SIZE: usize = 64;
 
 type Field<const N: usize, const M: usize> = [BitArray<[usize; N]>; M];
 
-fn get_triple_simd(values: [u8; 18]) -> (Simd<u8, 16>, Simd<u8, 16>) {
-    let alives = u8x16::from_slice(&values[1..=CHUNK_SIZE]);
+fn get_triple_simd(values: [u8; CHUNK_SIZE + 2]) -> (Simd<u8, CHUNK_SIZE>, Simd<u8, CHUNK_SIZE>) {
+    let alives = u8x64::from_slice(&values[1..=CHUNK_SIZE]);
     (
         alives,
-        u8x16::from_slice(&values[2..]) + alives + u8x16::from_slice(&values[..CHUNK_SIZE]),
+        u8x64::from_slice(&values[2..]) + alives + u8x64::from_slice(&values[..CHUNK_SIZE]),
     )
 }
 
@@ -94,27 +94,55 @@ macro_rules! get_simd {
             $arr[$i + 13][$j] as u8,
             $arr[$i + 14][$j] as u8,
             $arr[$i + 15][$j] as u8,
+            $arr[$i + 16][$j] as u8,
+            $arr[$i + 17][$j] as u8,
+            $arr[$i + 18][$j] as u8,
+            $arr[$i + 19][$j] as u8,
+            $arr[$i + 20][$j] as u8,
+            $arr[$i + 21][$j] as u8,
+            $arr[$i + 22][$j] as u8,
+            $arr[$i + 23][$j] as u8,
+            $arr[$i + 24][$j] as u8,
+            $arr[$i + 25][$j] as u8,
+            $arr[$i + 26][$j] as u8,
+            $arr[$i + 27][$j] as u8,
+            $arr[$i + 28][$j] as u8,
+            $arr[$i + 29][$j] as u8,
+            $arr[$i + 30][$j] as u8,
+            $arr[$i + 31][$j] as u8,
+            $arr[$i + 32][$j] as u8,
+            $arr[$i + 33][$j] as u8,
+            $arr[$i + 34][$j] as u8,
+            $arr[$i + 35][$j] as u8,
+            $arr[$i + 36][$j] as u8,
+            $arr[$i + 37][$j] as u8,
+            $arr[$i + 38][$j] as u8,
+            $arr[$i + 39][$j] as u8,
+            $arr[$i + 40][$j] as u8,
+            $arr[$i + 41][$j] as u8,
+            $arr[$i + 42][$j] as u8,
+            $arr[$i + 43][$j] as u8,
+            $arr[$i + 44][$j] as u8,
+            $arr[$i + 45][$j] as u8,
+            $arr[$i + 46][$j] as u8,
+            $arr[$i + 47][$j] as u8,
+            $arr[$i + 48][$j] as u8,
+            $arr[$i + 49][$j] as u8,
+            $arr[$i + 50][$j] as u8,
+            $arr[$i + 51][$j] as u8,
+            $arr[$i + 52][$j] as u8,
+            $arr[$i + 53][$j] as u8,
+            $arr[$i + 54][$j] as u8,
+            $arr[$i + 55][$j] as u8,
+            $arr[$i + 56][$j] as u8,
+            $arr[$i + 57][$j] as u8,
+            $arr[$i + 58][$j] as u8,
+            $arr[$i + 59][$j] as u8,
+            $arr[$i + 60][$j] as u8,
+            $arr[$i + 61][$j] as u8,
+            $arr[$i + 62][$j] as u8,
+            $arr[$i + 63][$j] as u8,
             $arr[$i_inc][$j] as u8,
-        ])
-    };
-    ($arr:ident($i:expr)[$j:expr]) => {
-        u8x16::from_array([
-            $arr[$i][$j] as u8,
-            $arr[$i + 1][$j] as u8,
-            $arr[$i + 2][$j] as u8,
-            $arr[$i + 3][$j] as u8,
-            $arr[$i + 4][$j] as u8,
-            $arr[$i + 5][$j] as u8,
-            $arr[$i + 6][$j] as u8,
-            $arr[$i + 7][$j] as u8,
-            $arr[$i + 8][$j] as u8,
-            $arr[$i + 9][$j] as u8,
-            $arr[$i + 10][$j] as u8,
-            $arr[$i + 11][$j] as u8,
-            $arr[$i + 12][$j] as u8,
-            $arr[$i + 13][$j] as u8,
-            $arr[$i + 14][$j] as u8,
-            $arr[$i + 15][$j] as u8,
         ])
     };
 }
