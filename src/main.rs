658552ecd50bbbd9f925138da6a327fce0146aad
Andrey Yakimenko <59168235+and-yakim@users.noreply.github.com>
2024-07-02 16:07:18 +0300

    add spherical boundry

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate kiss3d;
 
 use kiss3d::light::Light;
-use kiss3d::nalgebra::{Translation3, Vector3};
+use kiss3d::nalgebra::{Point3, Translation3, Vector3};
 use kiss3d::scene::SceneNode;
 use kiss3d::window::Window;
 
@@ -13,10 +13,14 @@ struct Molecule {
     position: Vector3<f32>,
 }
 
-const MASS: f32 = 1.0;
+// const MASS: f32 = 1.0;
 const RADIUS: f32 = 0.1;
-const EPSILON: f32 = 0.2;
+const EPSILON: f32 = 0.1;
 const SIGMA: f32 = 0.1;
+const DT: f32 = 0.01;
+const BOUNDRY_RADIUS: f32 = 1.0;
+const MIN_TEMP: f32 = 0.0;
+const MAX_TEMP: f32 = 0.1;
 
 impl Molecule {
     fn new(window: &mut Window, position: Vector3<f32>, velocity: Vector3<f32>) -> Self {
@@ -30,14 +34,28 @@ impl Molecule {
         }
     }
 
-    fn update_position(&mut self, dt: f32) {
-        self.position += self.velocity * dt;
+    fn update_state(&mut self) {
+        self.update_velocity_by_boundry();
+        self.update_position();
+        self.set_temperature();
+    }
+
+    fn update_velocity_by_boundry(&mut self) {
+        let pos_norm = self.position.norm();
+        let pos_dir = self.position.normalize();
+        let r = pos_dir * (BOUNDRY_RADIUS - pos_norm);
+        let force = lennard_jones_force(r, EPSILON, SIGMA);
+        self.velocity += force * DT;
+    }
+
+    fn update_position(&mut self) {
+        self.position += self.velocity * DT;
         self.sphere
             .set_local_translation(Translation3::from(self.position));
     }
 
-    fn set_temperature(&mut self, min_temp: f32, max_temp: f32) {
-        let (r, g, b) = temperature_to_color(self.velocity.norm_squared(), min_temp, max_temp);
+    fn set_temperature(&mut self) {
+        let (r, g, b) = temperature_to_color(self.velocity.norm_squared(), MIN_TEMP, MAX_TEMP);
         self.sphere.set_color(r, g, b);
     }
 }
@@ -55,20 +73,17 @@ fn temperature_to_color(temperature: f32, min_temp: f32, max_temp: f32) -> (f32,
 
 fn lennard_jones_force(r: Vector3<f32>, epsilon: f32, sigma: f32) -> Vector3<f32> {
     let r_norm = r.norm();
-    let lj_scalar = 24.0 * epsilon * (2.0 * (sigma / r_norm).powi(12) - (sigma / r_norm).powi(6))
-        / r_norm.powi(2);
+    let mupltiplier = (sigma / r_norm).powi(6);
+    let lj_scalar = 24.0 * epsilon * (2.0 * mupltiplier.powi(2) - mupltiplier) / r_norm.powi(2);
     r * lj_scalar
 }
 
-fn update_velocities(molecules: &mut [Molecule], i: usize, j: usize, dt: f32) {
+fn update_velocities(molecules: &mut [Molecule], i: usize, j: usize) {
     let r = molecules[j].position - molecules[i].position;
     let force = lennard_jones_force(r, EPSILON, SIGMA);
 
-    let acceleration1 = force / MASS;
-    let acceleration2 = -force / MASS;
-
-    molecules[i].velocity += acceleration1 * dt;
-    molecules[j].velocity += acceleration2 * dt;
+    molecules[i].velocity += force * DT;
+    molecules[j].velocity += -force * DT;
 }
 
 fn main() {
@@ -76,11 +91,9 @@ fn main() {
     window.set_light(Light::StickToCamera);
 
     let start_instant = time::Instant::now();
-    let time_limit = 5;
+    let time_limit = 15;
 
-    let dt = 0.01; // Time step
-
-    let n: usize = 3;
+    let n: usize = 6;
     let mut molecules = vec![
         Molecule::new(
             &mut window,
@@ -97,25 +110,36 @@ fn main() {
             Vector3::new(0.6, -0.1, 0.0),
             Vector3::new(-0.3, 0.0, 0.0),
         ),
+        Molecule::new(
+            &mut window,
+            Vector3::new(-0.8, 0.0, 0.0),
+            Vector3::new(0.1, 0.0, 0.0),
+        ),
+        Molecule::new(
+            &mut window,
+            Vector3::new(0.8, 0.05, 0.1),
+            Vector3::new(-0.2, 0.0, 0.0),
+        ),
+        Molecule::new(
+            &mut window,
+            Vector3::new(0.2, -0.1, 0.0),
+            Vector3::new(-0.3, 0.0, 0.0),
+        ),
     ];
 
-    let min_temp = 0.0;
-    let max_temp = molecules.iter().fold(min_temp, |acc, mol| {
-        f32::max(acc, mol.velocity.norm_squared())
-    });
+    let eye = Point3::new(BOUNDRY_RADIUS * 2.0, 0.0, 0.0);
+    let at = Point3::origin();
 
-    while window.render() {
+    while window.render_with_camera(&mut kiss3d::camera::ArcBall::new(eye, at)) {
         if start_instant.elapsed().as_secs() > time_limit {
             break;
         };
         for i in 0..(n - 1) {
             for j in (i + 1)..n {
-                update_velocities(&mut molecules, i, j, dt);
+                update_velocities(&mut molecules, i, j);
             }
-            molecules[i].set_temperature(min_temp, max_temp);
-            molecules[i].update_position(dt);
+            molecules[i].update_state();
         }
-        molecules[n - 1].set_temperature(min_temp, max_temp);
-        molecules[n - 1].update_position(dt);
+        molecules[n - 1].update_state();
     }
 }
