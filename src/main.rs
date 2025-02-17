950b42d0a4bf60d8bdb9d6bb6b5c3c83accc7aca
Andrey Yakimenko <59168235+and-yakim@users.noreply.github.com>
2025-02-17 19:32:51 +0200

    is this real /dev/null ?

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,31 @@
-fn main() {}
+use std::io::Write;
+use std::{fs::OpenOptions, time};
+
+const RANGE: usize = 1_000_000;
+
+fn fizzbuzz(n: usize) -> String {
+    match (n % 3, n % 5) {
+        (0, 0) => "FizzBuzz".to_string(),
+        (0, _) => "Fizz".to_string(),
+        (_, 0) => "Buzz".to_string(),
+        _ => n.to_string(),
+    }
+}
+
+fn print_fizzbuzz(range: usize) {
+    let mut file = OpenOptions::new().append(true).open("/dev/null").unwrap();
+    for i in 1..(range + 1) {
+        writeln!(file, "{}", fizzbuzz(i)).unwrap();
+        // println!("{}", fizzbuzz(i));
+    }
+}
+
+fn main() {
+    let instant = time::Instant::now();
+    print_fizzbuzz(RANGE);
+    let elapsed = instant.elapsed().as_micros();
+    let mut file = OpenOptions::new().append(true).open("src/main.rs").unwrap();
+    writeln!(file, "//Elapsed: {elapsed} µs").unwrap();
+    println!("Elapsed: {elapsed} µs");
+}
+
