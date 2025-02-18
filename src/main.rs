34e7842e78c0fa6a03edc620515cff4255398a5a
Andrey Yakimenko <59168235+and-yakim@users.noreply.github.com>
2025-02-18 04:19:51 +0200

    -

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,11 @@ use std::{fs::OpenOptions, time};
 const RANGE: usize = 1_000_000;
 const STEP: usize = 15;
 
-//(i, len): [(1, 58), (16, 63), (91, 66), (106, 71), (991, 74), (1006, 79), (9991, 82), (10006, 87), (99991, 90), (100006, 95), (999991, 98), (1000006, 103), (9999991, 106), (10000006, 111), (99999991, 114), (100000006, 119), (999999991, 122)]
-fn long_fizzbuzz(n: usize) -> Vec<u8> {
-    assert!(n % STEP == 1);
+// const SIZES: [(usize, usize); 17] = [(1, 58), (16, 63), (91, 66), (106, 71), (991, 74), (1006, 79), (9991, 82), (10006, 87), (99991, 90), (100006, 95), (999991, 98), (1000006, 103), (9999991, 106), (10000006, 111), (99999991, 114), (100000006, 119), (999999991, 122)]; // (i, len)
 
-    let mut buffer: Vec<u8> = Vec::with_capacity(98);
+fn write_fizzbuzz_inlined(n: usize, buffer: &mut Vec<u8>) {
     write!(
-        &mut buffer,
+        buffer,
         "{}\n{}\nFizz\n{}\nBuzz\nFizz\n{}\n{}\nFizz\nBuzz\n{}\nFizz\n{}\n{}\nFizzBuzz\n",
         n,
         n + 1,
@@ -22,6 +20,13 @@ fn long_fizzbuzz(n: usize) -> Vec<u8> {
         n + 13
     )
     .unwrap();
+}
+
+fn long_fizzbuzz(n: usize) -> Vec<u8> {
+    assert!(n % STEP == 1);
+
+    let mut buffer: Vec<u8> = Vec::with_capacity(95);
+    write_fizzbuzz_inlined(n, &mut buffer);
 
     buffer
 }
@@ -38,7 +43,7 @@ fn fizzbuzz(n: usize) -> String {
 fn print_fizzbuzz(range: usize) {
     let mut file = OpenOptions::new().append(true).open("/dev/null").unwrap();
     for i in (1..(range + 1 - range % STEP)).step_by(STEP) {
-        writeln!(file, "{}", String::from_utf8_lossy(&long_fizzbuzz(i))).unwrap();
+        write!(file, "{}", String::from_utf8_lossy(&long_fizzbuzz(i))).unwrap();
         // print!("{}", String::from_utf8_lossy(&long_fizzbuzz(i)));
     }
     for i in (range + 1 - range % STEP)..(range + 1) {
@@ -54,19 +59,8 @@ fn main() {
     let mut file = OpenOptions::new().append(true).open("src/main.rs").unwrap();
     writeln!(file, "//Elapsed: {elapsed} µs").unwrap();
     println!("Elapsed: {elapsed} µs");
-
-    // let mut sizes: Vec<(usize, usize)> = Vec::new();
-    // let mut max_length = 0;
-    // for i in (1..RANGE).step_by(STEP) {
-    //     let length = long_fizzbuzz(i).len();
-    //     if length > max_length {
-    //         sizes.push((i, length));
-    //         max_length = length;
-    //     }
-    // }
-    // println!("{:?}", sizes);
 }
 
-//Elapsed: 907474 µs - classic 1M /dev/null
-//Elapsed: 175508 µs - long_fizzbuzz
-//Elapsed: 127444 µs - Vec::with_capacity(98)
+//Elapsed: 907 ms - classic 1M /dev/null
+//Elapsed: 175 ms - long_fizzbuzz
+//Elapsed: 127 ms - Vec::with_capacity(95)
