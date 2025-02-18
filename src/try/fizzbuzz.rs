310772cc93874eba392a82f5331f1595b91398dd
Andrey Yakimenko <59168235+and-yakim@users.noreply.github.com>
2025-02-18 14:22:16 +0200

    save stable fizzbuzz

--- src/try/fizzbuzz.rs
+++ src/try/fizzbuzz.rs
@@ -4,46 +4,59 @@ use std::{fs::OpenOptions, time};
 const RANGE: usize = 1_000_000;
 const STEP: usize = 15;
 
-//(i, len): [(1, 58), (16, 63), (91, 66), (106, 71), (991, 74), (1006, 79), (9991, 82), (10006, 87), (99991, 90), (100006, 95), (999991, 98), (1000006, 103), (9999991, 106), (10000006, 111), (99999991, 114), (100000006, 119), (999999991, 122)]
-fn long_fizzbuzz(n: usize) -> Vec<u8> {
-    assert!(n % STEP == 1);
-
-    let mut buffer: Vec<u8> = Vec::new();
-    write!(
-        &mut buffer,
+fn write_fizzbuzz_inlined(i: usize, buffer: &mut String) {
+    assert!(i % STEP == 1);
+    let string = format!(
         "{}\n{}\nFizz\n{}\nBuzz\nFizz\n{}\n{}\nFizz\nBuzz\n{}\nFizz\n{}\n{}\nFizzBuzz\n",
-        n,
-        n + 1,
-        n + 3,
-        n + 6,
-        n + 7,
-        n + 10,
-        n + 12,
-        n + 13
-    )
-    .unwrap();
-
-    buffer
+        i,
+        i + 1,
+        i + 3,
+        i + 6,
+        i + 7,
+        i + 10,
+        i + 12,
+        i + 13
+    );
+    buffer.push_str(&string);
 }
 
 fn fizzbuzz(n: usize) -> String {
     match (n % 3, n % 5) {
-        (0, 0) => "FizzBuzz".to_string(),
-        (0, _) => "Fizz".to_string(),
-        (_, 0) => "Buzz".to_string(),
-        _ => n.to_string(),
+        (0, 0) => String::from("FizzBuzz\n"),
+        (0, _) => String::from("Fizz\n"),
+        (_, 0) => String::from("Buzz\n"),
+        _ => String::from(n.to_string() + "\n"),
     }
+}
+
+const BUFFER: usize = 1024;
+
+fn string_size(n: usize) -> usize {
+    (n as f64).log10().floor() as usize + 1
+}
+
+fn sizecap(i: usize) -> usize {
+    BUFFER - (50 + 8 * string_size(i))
 }
 
 fn print_fizzbuzz(range: usize) {
     let mut file = OpenOptions::new().append(true).open("/dev/null").unwrap();
-    for i in (1..range).step_by(STEP) {
-        writeln!(file, "{}", String::from_utf8_lossy(&long_fizzbuzz(i))).unwrap();
-        // print!("{}", String::from_utf8_lossy(&long_fizzbuzz(i)));
+    let mut buffer = String::with_capacity(BUFFER);
+
+    for i in (1..(range + 1 - range % STEP)).step_by(STEP) {
+        write_fizzbuzz_inlined(i, &mut buffer);
+        if buffer.len() > sizecap(i) {
+            write!(file, "{}", &buffer).unwrap();
+            // print!("{}", &buffer);
+            buffer.clear();
+        }
     }
     for i in (range + 1 - range % STEP)..(range + 1) {
-        writeln!(file, "{}", fizzbuzz(i)).unwrap();
-        // println!("{}", fizzbuzz(i));
+        buffer.push_str(&fizzbuzz(i));
+    }
+    if buffer.len() > 0 {
+        write!(file, "{}", &buffer).unwrap();
+        // print!("{}", &buffer);
     }
 }
 
@@ -54,18 +67,11 @@ fn main() {
     let mut file = OpenOptions::new().append(true).open("src/main.rs").unwrap();
     writeln!(file, "//Elapsed: {elapsed} µs").unwrap();
     println!("Elapsed: {elapsed} µs");
-
-    // let mut sizes: Vec<(usize, usize)> = Vec::new();
-    // let mut max_length = 0;
-    // for i in (1..RANGE).step_by(STEP) {
-    //     let length = long_fizzbuzz(i).len();
-    //     if length > max_length {
-    //         sizes.push((i, length));
-    //         max_length = length;
-    //     }
-    // }
-    // println!("{:?}", sizes);
 }
 
-//Elapsed: 907474 µs - classic 1M /dev/null
-//Elapsed: 175508 µs - long_fizzbuzz
+//Time:    907 ms - classic 1M /dev/null
+//Time:    175 ms - long_fizzbuzz
+//Time:    127 ms - Vec::with_capacity(95)
+//Time:    107 ms - reuse same buffer
+//Time:     74 ms - 1kb buffer
+//Time:     45 ms - fix bug and String
