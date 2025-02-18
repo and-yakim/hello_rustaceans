95a79dbd70d3fe51a3550e2b290f22e5e5e76973
Andrey Yakimenko <59168235+and-yakim@users.noreply.github.com>
2025-02-18 04:54:47 +0200

    reuse buffer

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,21 @@ const STEP: usize = 15;
 
 // const SIZES: [(usize, usize); 17] = [(1, 58), (16, 63), (91, 66), (106, 71), (991, 74), (1006, 79), (9991, 82), (10006, 87), (99991, 90), (100006, 95), (999991, 98), (1000006, 103), (9999991, 106), (10000006, 111), (99999991, 114), (100000006, 119), (999999991, 122)]; // (i, len)
 
-fn write_fizzbuzz_inlined(n: usize, buffer: &mut Vec<u8>) {
+fn write_fizzbuzz_inlined(i: usize, buffer: &mut Vec<u8>) {
+    assert!(i % STEP == 1);
     write!(
         buffer,
         "{}\n{}\nFizz\n{}\nBuzz\nFizz\n{}\n{}\nFizz\nBuzz\n{}\nFizz\n{}\n{}\nFizzBuzz\n",
-        n,
-        n + 1,
-        n + 3,
-        n + 6,
-        n + 7,
-        n + 10,
-        n + 12,
-        n + 13
+        i,
+        i + 1,
+        i + 3,
+        i + 6,
+        i + 7,
+        i + 10,
+        i + 12,
+        i + 13
     )
     .unwrap();
-}
-
-fn long_fizzbuzz(n: usize) -> Vec<u8> {
-    assert!(n % STEP == 1);
-
-    let mut buffer: Vec<u8> = Vec::with_capacity(95);
-    write_fizzbuzz_inlined(n, &mut buffer);
-
-    buffer
 }
 
 fn fizzbuzz(n: usize) -> String {
@@ -42,9 +34,13 @@ fn fizzbuzz(n: usize) -> String {
 
 fn print_fizzbuzz(range: usize) {
     let mut file = OpenOptions::new().append(true).open("/dev/null").unwrap();
+    let mut buffer: Vec<u8> = Vec::with_capacity(95);
+
     for i in (1..(range + 1 - range % STEP)).step_by(STEP) {
-        write!(file, "{}", String::from_utf8_lossy(&long_fizzbuzz(i))).unwrap();
-        // print!("{}", String::from_utf8_lossy(&long_fizzbuzz(i)));
+        write_fizzbuzz_inlined(i, &mut buffer);
+        write!(file, "{}", String::from_utf8_lossy(&buffer)).unwrap();
+        // print!("{}", String::from_utf8_lossy(&buffer));
+        buffer.clear();
     }
     for i in (range + 1 - range % STEP)..(range + 1) {
         writeln!(file, "{}", fizzbuzz(i)).unwrap();
@@ -64,3 +60,4 @@ fn main() {
 //Elapsed: 907 ms - classic 1M /dev/null
 //Elapsed: 175 ms - long_fizzbuzz
 //Elapsed: 127 ms - Vec::with_capacity(95)
+//Elapsed: 107 ms - reuse same buffer
