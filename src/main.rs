365d21eb86ad71ada34cd28ef061e7b36a669ed0
Andrey Yakimenko <59168235+and-yakim@users.noreply.github.com>
2025-02-23 03:27:21 +0200

    add trait for custon vec

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,69 @@
-use std::collections::LinkedList;
+use std::{collections::LinkedList, ops::Add, time};
 
 use macroquad::prelude::*;
 
-const COLS: u32 = 30;
-const ROWS: u32 = 20;
+const COLS: i32 = 30;
+const ROWS: i32 = 20;
 
 const CELL: f32 = 20.;
 const WIDTH: f32 = COLS as f32 * CELL;
 const HEIGHT: f32 = ROWS as f32 * CELL;
 
-// enum DIR {
-//     UP,
-//     DOWN,
-//     LEFT,
-//     RIGHT,
-// }
+const TIMESTEP: u128 = 200; // ms
 
-fn draw_cell(offset: Vec2, cell: &UVec2, color: Color) {
+enum Dir {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Copy, Clone)]
+struct Vec2_ {
+    x: i32,
+    y: i32,
+}
+
+impl Vec2_ {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+}
+
+impl Add for Vec2_ {
+    type Output = Vec2_;
+
+    fn add(self, rhs: Vec2_) -> Vec2_ {
+        Vec2_ {
+            x: (self.x + rhs.x + COLS) % COLS,
+            y: (self.y + rhs.y + ROWS) % ROWS,
+        }
+    }
+}
+
+impl Add for &Vec2_ {
+    type Output = Vec2_;
+
+    fn add(self, rhs: &Vec2_) -> Vec2_ {
+        Vec2_ {
+            x: (self.x + rhs.x + COLS) % COLS,
+            y: (self.y + rhs.y + ROWS) % ROWS,
+        }
+    }
+}
+
+impl Dir {
+    fn to_ivec2(&self) -> Vec2_ {
+        match self {
+            Dir::Up => Vec2_ { x: 0, y: -1 },
+            Dir::Down => Vec2_ { x: 0, y: 1 },
+            Dir::Left => Vec2_ { x: -1, y: 0 },
+            Dir::Right => Vec2_ { x: 1, y: 0 },
+        }
+    }
+}
+
+fn draw_cell(offset: Vec2, cell: &Vec2_, color: Color) {
     draw_rectangle(
         offset.x + cell.x as f32 * CELL,
         offset.y + cell.y as f32 * CELL,
@@ -28,16 +75,37 @@ fn draw_cell(offset: Vec2, cell: &UVec2, color: Color) {
 
 #[macroquad::main("Hello World")]
 async fn main() {
-    let mut snake: LinkedList<UVec2> = LinkedList::new();
+    // let mut field: [bool; ROWS * COLS] = []; // i can afford some memory not to redraw twice a cell // true means snake
+    let mut snake: LinkedList<Vec2_> = LinkedList::new();
     let mut score = 0;
 
-    // let mut dir_prev = DIR::RIGHT;
-    // let mut dir_next = DIR::RIGHT;
+    let mut dir_prev = Dir::Right;
+    let mut dir_next = Dir::Right;
 
-    let mut food = uvec2(COLS as u32 / 2, ROWS as u32 / 2);
-    snake.push_back(food - uvec2(2, 4));
+    let mut food = Vec2_::new(COLS as i32 / 2, ROWS as i32 / 2);
+    snake.push_front(food + Vec2_::new(-2, -4));
+    snake.push_front(food + Vec2_::new(-1, -4));
+
+    let mut instant = time::Instant::now();
 
     loop {
+        let elapsed = instant.elapsed().as_millis();
+        if elapsed > TIMESTEP {
+            // ingame time needed
+
+            // use dir_prev to push_front
+            // check for food and collision
+            // refresh food or tail
+
+            // input handling
+            // should swirl diagonally on simultaneous press
+            if let Some(_) = get_last_key_pressed() {
+                let _ = snake.pop_back().unwrap();
+                snake.push_front(snake.front().unwrap() + &dir_next.to_ivec2());
+                instant = time::Instant::now();
+            }
+        }
+
         clear_background(DARKGRAY);
 
         let text = format!("Score: {}", score);
@@ -50,22 +118,15 @@ async fn main() {
 
         for x in 0..COLS {
             for y in 0..ROWS {
-                draw_cell(offset, &uvec2(x, y), GRAY);
+                draw_cell(offset, &Vec2_::new(x, y), GRAY);
             }
         }
-
-        // use dir_prev to push_front
-        // check for food and collision
-        // refresh food or tail
 
         for cell in &snake {
             draw_cell(offset, &cell, DARKPURPLE);
         }
 
         draw_cell(offset, &food, GOLD);
-
-        // input handling
-        // should swirl diagonally on simultaneous press
 
         if is_key_pressed(KeyCode::Escape) {
             break;
