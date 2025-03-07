7d5053546b40e52e6b207e28fe832faf67384244
Andrey Yakimenko <59168235+and-yakim@users.noreply.github.com>
2025-03-07 20:36:21 +0200

    fix render pixelated

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,26 @@ use macroquad::prelude::*;
 
 const TIMESTEP: u128 = 250; // ms
 
-const UNIT: f32 = 16.0;
+const SPRITE: f32 = 16.0;
 const SCALE: f32 = 4.0;
-const SIZE: Vec2 = vec2(UNIT * SCALE, UNIT * SCALE - 1.0);
+
+const UNIT: f32 = SPRITE * SCALE;
+const SIZE: Vec2 = vec2(UNIT, UNIT);
 
 fn get_source_rect(x: usize, y: usize) -> Rect {
-    Rect::new(UNIT * x as f32, UNIT * y as f32 + 1.0, UNIT, UNIT - 0.5)
+    Rect::new(SPRITE * x as f32, SPRITE * y as f32, SPRITE, SPRITE)
 }
 
 #[macroquad::main("Platformer")]
 async fn main() {
     let mut instant = time::Instant::now();
+    let (width, height) = (screen_width(), screen_height());
 
     let sprite = Texture2D::from_file_with_format(include_bytes!("sprite.png"), None);
+    sprite.set_filter(FilterMode::Nearest);
     let params = DrawTextureParams {
         dest_size: Some(SIZE),
-        source: Some(get_source_rect(4, 2)),
+        source: Some(get_source_rect(10, 2)),
         rotation: 0.0,
         flip_x: false,
         flip_y: false,
@@ -33,7 +37,13 @@ async fn main() {
 
         clear_background(DARKGRAY);
 
-        draw_texture_ex(&sprite, UNIT, UNIT, WHITE, params.clone());
+        draw_texture_ex(
+            &sprite,
+            (width - UNIT) / 2.0,
+            (height - UNIT) / 2.0,
+            WHITE,
+            params.clone(),
+        );
 
         if is_key_pressed(KeyCode::Escape) {
             break;
